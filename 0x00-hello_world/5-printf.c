80760e78c8712a69f4878e3a2179ddb109d6a5ea
bryansine <bryansine1738@gmail.com>
2023-06-15 05:18:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -a program that prints a line using printf function
+ * return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
