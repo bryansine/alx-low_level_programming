6bc1d2f116e51f87e71d832fb44ef2506dd7ffc4
bryansine <bryansine1738@gmail.com>
2023-08-01 06:56:58 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,15 +3,15 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * 
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ */
+
 typedef struct listint_s
 {
 	    int n;
