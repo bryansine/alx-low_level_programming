126c9218364794c648cf7062ebb035e4770fe553
bryansine <bryansine1738@gmail.com>
2023-10-16 06:03:39 -0700

    task 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -19,17 +19,3 @@ int mydiv(int a, int b) {
 int mymod(int a, int b) {
     return a % b;
 }
-
-int main() {
-    int x = 10;
-    int y = 5;
-
-    printf("Sum: %d\n", add(x, y));
-    printf("Difference: %d\n", sub(x, y));
-    printf("Product: %d\n", mul(x, y));
-    printf("Division: %d\n", mydiv(x, y));
-    printf("Modulus: %d\n", mymod(x, y));
-
-    return 0;
-}
-
